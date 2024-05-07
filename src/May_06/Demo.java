2984889f81ac65c3316b28e47f85212c2c1fee2d
shashwat-verma-hm <shashwat.verma@hm.com>
2024-05-07 11:54:32 +0530

    first commit

---
+++ src/May_06/Demo.java
@@ -0,0 +1,10 @@
+package May_06;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Welcome to Java Class!");
+	}
+
+}
---
+++ src/May_07/Demo4.java
@@ -0,0 +1,10 @@
+package May_07;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
